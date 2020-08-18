3210c62e50913e8b28e2f5101c01e057dc26b8e5
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-08-18 23:37:21 +0100

    feat: Add integration with org-roam
    Current limitations:
    * Can't set `org-roam-tag-sources` despite being set correctly
      (it seems..)

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((eval . (setq-local org-roam-directory (concat (file-name-as-directory (projectile-project-root)) "org")))
+         (org-roam-file-exclude-regexp . "_index.org")))
+ ("org" . ((org-mode . ((eval . (org-hugo-auto-export-mode 1)))))))
+

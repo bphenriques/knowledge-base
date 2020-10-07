7d8dda38d62a8106ccefdfa088ba3cade48c298d
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2020-10-07 22:50:44 +0100

    Iterate on akka systems

--- tools/versions/default.el
+++ tools/versions/default.el
@@ -1,7 +1,7 @@
 ; Must be consistent with: https://github.com/bphenriques/dotfiles/blob/master/emacs/.config/doom/packages.el
 (
  ; https://github.com/org-roam/org-roam
- ("org-roam" . "369753c")
+ ("org-roam" . "0cce9d116580665d9eb9284d3317ae1beda56bc1")
 
  ; https://github.com/kaushalmodi/ox-hugo
  ("ox-hugo" . "95723cd"))

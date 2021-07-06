064de3706d6486e3484eda0966a6a13b4d78312e
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2021-07-06 21:37:32 +0100

    Reogarnize org content

--- tools/versions/default.el
+++ tools/versions/default.el
@@ -1,5 +1,5 @@
 ; Must be consistent with: https://github.com/bphenriques/dotfiles/blob/master/emacs/.config/doom/packages.el
 (
  ; https://github.com/kaushalmodi/ox-hugo
- ("ox-hugo" . "95723cd"))
+ ("ox-hugo" . "290b5d6"))
 :beta

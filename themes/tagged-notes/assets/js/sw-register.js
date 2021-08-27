0cfa57b414aa6a7ffcfc7a2fc4ad870f78c11ab9
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2021-08-27 10:01:07 +0100

    Remove service worker (may add in the future)

--- themes/tagged-notes/assets/js/sw-register.js
+++
@@ -1,7 +0,0 @@
-{{- $swJS := resources.Get "js/sw.js" | resources.ExecuteAsTemplate "js/sw.js" . -}}
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register(
-    "{{ $swJS.RelPermalink }}", 
-    { scope: "{{ "/" | relURL }}" }
-  );
-}

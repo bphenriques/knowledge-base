5767c2d4b96459584a274002bfa903dfb92a6d41
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2022-02-17 22:01:34 +0000

    Add integration with Nix for ephemeral dev environment

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "bphenriques's knowledge base";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShell = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}

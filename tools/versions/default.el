4f5728a7e7e2b383d499c4c15ed0566d2c5ba382
Bruno Henriques <4727729+bphenriques@users.noreply.github.com>
2022-04-28 15:05:36 +0100

    chore: Bump ox-hugo

--- tools/versions/default.el
+++ tools/versions/default.el
@@ -1,5 +1,5 @@
 ; Must be consistent with: https://github.com/bphenriques/dotfiles/blob/master/emacs/.config/doom/packages.el
 (
  ; https://github.com/kaushalmodi/ox-hugo
- ("ox-hugo" . "de5dfdfdcdc3ec50cb27d1f6cf7314a5c9aa1385"))
+ ("ox-hugo" . "262b7b432a7f81124fe181c07b57a4f42b6eedc9"))
 :beta
